3eac73153935fbb808e41a172d4abf53a4fdf3eb
Harold Hannon <haroldhannon@gmail.com>
2022-03-22 10:27:28 -0500

    Add files via upload
    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vrtManager',
+    version='0.1',
+    packages=['vrtManager'],
+    url='',
+    license='none',
+    author='haroldhannon',
+    author_email='hhannon@us.ibm.com',
+    description='quick libvirt wrapper library'
+)
